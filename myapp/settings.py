4a9ffaad14404700aaf2b5467bd64e4b9c790a62
Swathiprakash06 <swathi06082004@gmail.com>
2026-01-21 15:19:59 +0530

    Update settings and add gunicorn

--- myapp/settings.py
+++ myapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l&v3)$@3ga_-5q5gm2i0*i9i&jihx+t8mdb0sp*^8r9=9jf2m(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
